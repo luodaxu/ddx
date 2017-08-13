9b594969716b8ab2645c865cb744b1fa1b6242fc
luoxu <luox199111@gmail.com>
2017-08-13 10:14:34 +0800

    remove annotation from routes.js

--- app/src/routes.js
+++ app/src/routes.js
@@ -4,7 +4,6 @@
 import React from 'react';
 import { Route } from 'react-router';
 import rootContainer from './containers/rootContainer';
-// import indexContainer from './containers/indexContainer';
 import loginContainer from './containers/loginContainer';
 import createContainer from './containers/createContainer';
 import postContainer from './containers/postContainer';
